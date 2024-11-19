4a3c147941fc61c1edb85331eba8125661b5c569
manish-maller-se <sesa768583@se.com>
2024-11-19 14:51:57 +0530

    Delete code_samples directory

--- code_samples/others/helloworld.py
+++
@@ -1,7 +0,0 @@
-# python program to print "Hello World"
-print("Hello World")
-
-#Add Two Numbers
-x = 5
-y = 10
-print(x + y)
--- code_samples/others/helloworld.rb
+++
@@ -1,9 +0,0 @@
-puts "Hello World"
-
-def palindrome?(string)
-    if string == string.reverse
-      return true
-    else
-      puts “String is not a palindrome”
-    end
-end
