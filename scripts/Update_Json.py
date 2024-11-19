329cca64009a0bd9992ef35ba7a309e5d8a114bc
manish-maller-se <sesa768583@se.com>
2024-11-19 15:26:51 +0530

    Update Update_Json.py

--- scripts/Update_Json.py
+++ scripts/Update_Json.py
@@ -21,13 +21,12 @@ INPUT_YAML_FILE = "original.yaml"
 
 with open(INPUT_MD_FILE, 'r', encoding='utf-8-sig') as md_file:
     md_file_data = md_file.read()
-md_data_append = f"""|- {md_file_data}"""
 
 with open(INPUT_YAML_FILE, 'r', encoding='utf-8-sig') as yaml_file:
     yaml_data = yaml.safe_load(yaml_file)
 
 for data in yaml_data:
     if data == 'info':
-        yaml_data[data].update({'description':md_data_append})
+        yaml_data[data].update({'description':md_file_data})
     with open(INPUT_YAML_FILE, 'w',encoding='utf-8-sig') as yaml_file:
         yaml.dump(yaml_data , yaml_file, sort_keys=False)
